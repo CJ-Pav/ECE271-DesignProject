b10b6e5084dfefdaf2ebc92fd65b011614883dd0
CJ-Pav <cpav@live.com>
2017-12-01 17:48:52 -0800

    bLUT2 fix

--- buttonLUT2.sv
+++ buttonLUT2.sv
@@ -9,15 +9,15 @@ module bLUT2(
 
     always_comb
         case (buttons)
-			'b00000001: buttons = 'b011011100; //Key1
-			'b00000010: buttons = 'b011110111; //Key2
-			'b00000011: buttons = 'b100000110; //Key3 
-			'b00000100: buttons = 'b100100110; //Key4
-			'b00000101: buttons = 'b101001010; //Key5
-			'b00000110: buttons = 'b101011101; //Key6
-			'b00000111: buttons = 'b110001000; //Key7
-			'b00001000: buttons = 'b110111000; //Key8
-			'b00001001: buttons = 'b000000000; //Key9 (pause)
+			'b00000001: buttonFreq = 'b011011100; //Key1
+			'b00000010: buttonFreq = 'b011110111; //Key2
+			'b00000011: buttonFreq = 'b100000110; //Key3 
+			'b00000100: buttonFreq = 'b100100110; //Key4
+			'b00000101: buttonFreq = 'b101001010; //Key5
+			'b00000110: buttonFreq = 'b101011101; //Key6
+			'b00000111: buttonFreq = 'b110001000; //Key7
+			'b00001000: buttonFreq = 'b110111000; //Key8
+			'b00001001: buttonFreq = 'b000000000; //Key9 (pause)
 		endcase
 
 endmodule
